df308647f5bf9fd6f8885ffc308c839f194ac9d9
Rennan <rennanmax80@gmail.com>
2021-03-21 18:18:22 -0300

    Componentização Sidebar e Content

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SidebarProps {
+  setNewGenreId: (id: number) => void;
+  selectedGenreId: number;
+}
+
+
+export function SideBar({setNewGenreId, selectedGenreId}: SidebarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  // function handleClickButton(id: number) {
+  //   setSelectedGenreId(id);
+  // }
+
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => setNewGenreId(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
+}
